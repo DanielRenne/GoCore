91cd1b190e493d94fe4be72e34ae3e161318dd8b
Dan <DRenne@hrscontrol.com>
2016-04-15 20:57:21 -0400

    Changed file case

---
+++ buildDB.go
@@ -0,0 +1,1 @@
+BuildDB.go
---
+++ getCore.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"core/extensions"
+	"core/zip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const goCoreURL = "https://github.com/DanielRenne/GoCore/archive/master.zip"
+const fileName = "goCoreMaster.zip"
+const gitRepoName = "GoCore-master"
+
+func main() {
+
+	out, errCreateFile := os.Create(fileName)
+
+	if errCreateFile != nil {
+		fmt.Println("Failed to create file handle:  " + errCreateFile.Error())
+		return
+	}
+
+	fmt.Println("Downloading GoCore.  Please Wait . . .")
+
+	resp, errHttpGet := http.Get(goCoreURL)
+	defer resp.Body.Close()
+
+	if errHttpGet != nil {
+		fmt.Println("Failed to Download GoCore master zip file:  " + errHttpGet.Error())
+		return
+	}
+
+	n, errCopyOut := io.Copy(out, resp.Body)
+
+	if errCopyOut != nil {
+		fmt.Println("Failed to Output to goCoreMaster.zip:  " + errCopyOut.Error())
+		return
+	}
+	out.Close()
+
+	fmt.Println("Downloaded GoCore Master successfully:  " + extensions.PrintMegaBytes(n))
+
+	fmt.Println("Unzipping goCoreMaster.zip . . .")
+
+	excludedFiles := []string{gitRepoName + "/webConfig.json"}
+	excludedFiles = append(excludedFiles, gitRepoName+"/src/core/app/app.go")
+	excludedFiles = append(excludedFiles, gitRepoName+"/keys/cert.pem")
+	excludedFiles = append(excludedFiles, gitRepoName+"/keys/key.pem")
+
+	errUnzip := zip.Unzip(fileName, "goCoreMaster", excludedFiles)
+
+	if errUnzip != nil {
+		fmt.Println("Failed to Unzip goCoreMaster.zip:  " + errUnzip.Error())
+		return
+	}
+
+	fmt.Println("Unzipped GoCore Master successfully.")
+
+	errRemoveRepoZip := os.Remove(fileName)
+
+	if errRemoveRepoZip != nil {
+		fmt.Println("Failed to Remove GoCore Repo Zip File:  " + errRemoveRepoZip.Error())
+		return
+	}
+
+	fmt.Println("Moving Files . . .")
+
+	dir, _ := os.Getwd()
+	dir = strings.Replace(dir, " ", "\\ ", -1)
+
+	//Copy the Files then Remove the Directory
+	extensions.CopyFolder("goCoreMaster/"+gitRepoName, dir)
+
+	fmt.Println("Moved Files Successfully.")
+	fmt.Println("Started Cleaning Files.")
+	errRemoveDecompressedFiles := extensions.RemoveDirectory("goCoreMaster")
+
+	if errRemoveDecompressedFiles != nil {
+		fmt.Println("Failed to Remove GoCore Decompressed Files:  " + errRemoveDecompressedFiles.Error())
+		return
+	}
+
+	fmt.Println("Cleaned up Repo Files Successfully.")
+}
