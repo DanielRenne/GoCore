4e73f6bb2a751740f565972804a2139a6bf1457f
Dan <DRenne@hrscontrol.com>
2016-05-04 08:06:59 -0400

    update tiedotTest app

--- src/tieDotTest/tiedotTest.go
+++ src/tieDotTest/tiedotTest.go
@@ -1,10 +1,28 @@
 package tieDotTest
 
 import (
-	// "fmt"
-	_ "tieDotTest/model"
+	"fmt"
+	"github.com/fatih/color"
+	"tieDotTest/model"
 )
 
 func init() {
 	// fmt.Printf("%+v\n", model)
+	var collection model.TestCollections
+	tc := model.TestCollection{Field1: 123}
+
+	docID, err := collection.Insert(tc)
+	if err != nil {
+		color.Red("Error inserting new TestCollection:  " + err.Error())
+		return
+	}
+
+	fmt.Println(string(docID))
+
+	readBack, err := collection.ReadById(docID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Document", docID)
+	fmt.Printf("%+v\n", readBack)
 }
