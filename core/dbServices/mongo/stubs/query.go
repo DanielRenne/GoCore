0cfd42a5d13334c3a909a98b8193f3c8889109ec
Daniel Renne <Daniel.Renne@atlona.com>
2016-09-05 10:44:21 -0400

    Refactored query and added In

--- core/dbServices/mongo/stubs/query.go
+++ core/dbServices/mongo/stubs/query.go
@@ -26,37 +26,31 @@ type Query struct {
 	e          error
 }
 
-func (self *Query) ById(id interface{}, x interface{}) error {
+func (self *Query) ById(val interface{}, x interface{}) error {
 
-	myIdType := reflect.TypeOf(id)
-	myIdInstance := reflect.ValueOf(id)
-	myIdInstanceHex := myIdInstance.MethodByName("Hex")
-	if myIdType.Name() != "ObjectId" && myIdType.Kind() == reflect.String && id.(string) != "" {
-		self.q = self.collection.FindId(bson.ObjectIdHex(id.(string)))
-	} else if myIdType.Name() == "ObjectId" && myIdType.Kind() == reflect.String && id.(string) != "" {
-		self.q = self.collection.FindId(bson.ObjectIdHex(myIdInstanceHex.Call([]reflect.Value{})[0].String()))
-	} else {
-		return errors.New(ERROR_INVALID_ID_VALUE)
+	objId, err := self.getIdHex(val)
+	if err != nil {
+		return err
 	}
 
+	self.q = self.collection.FindId(objId)
 	return self.q.One(x)
 
 }
 
 func (self *Query) Filter(criteria map[string]interface{}) *Query {
 
-	idVal, hasId := criteria["Id"]
+	val, hasId := criteria["Id"]
 	if hasId {
-		myIdType := reflect.TypeOf(idVal)
-		myIdInstance := reflect.ValueOf(idVal)
-		myIdInstanceHex := myIdInstance.MethodByName("Hex")
-		if myIdType.Name() != "ObjectId" && myIdType.Kind() == reflect.String {
-			self.q = self.collection.FindId(bson.ObjectIdHex(idVal.(string)))
-		} else if myIdType.Name() == "ObjectId" && myIdType.Kind() == reflect.String {
-			self.q = self.collection.FindId(bson.ObjectIdHex(myIdInstanceHex.Call([]reflect.Value{})[0].String()))
-		} else {
-			self.e = errors.New(ERROR_INVALID_ID_VALUE)
+
+		objId, err := self.getIdHex(val)
+		if err != nil {
+			self.e = err
+			return self
 		}
+
+		self.q = self.collection.FindId(objId)
+
 		return self
 	} else {
 
@@ -72,6 +66,32 @@ func (self *Query) Filter(criteria map[string]interface{}) *Query {
 
 	}
 	return self
+}
+
+func (self *Query) In(field string, values ...string) *Query {
+
+	if field == "Id" {
+		var ids []bson.ObjectId
+
+		for _, val := range values {
+
+			objId, err := self.getIdHex(val)
+			if err != nil {
+				self.e = err
+				continue
+			}
+
+			ids = append(ids, objId)
+		}
+
+		self.q = self.collection.Find(bson.M{"_id": bson.M{"$in": ids}})
+
+	} else {
+
+		self.q = self.collection.Find(bson.M{field: bson.M{"$in": values}})
+	}
+	return self
+
 }
 
 func (self *Query) Range(criteria map[string]Range) *Query {
@@ -105,21 +125,41 @@ func (self *Query) Skip(val int) *Query {
 }
 
 func (self *Query) All(x interface{}) error {
+
+	if self.e != nil {
+		return self.e
+	}
+
 	q := self.generateQuery()
 	return q.All(x)
 }
 
 func (self *Query) One(x interface{}) error {
+
+	if self.e != nil {
+		return self.e
+	}
+
 	q := self.generateQuery()
 	return q.One(x)
 }
 
 func (self *Query) Count(x interface{}) (int, error) {
+
+	if self.e != nil {
+		return 0, self.e
+	}
+
 	q := self.generateQuery()
 	return q.Count()
 }
 
 func (self *Query) Distinct(key string, x interface{}) error {
+
+	if self.e != nil {
+		return self.e
+	}
+
 	q := self.generateQuery()
 	return q.Distinct(key, x)
 }
@@ -149,4 +189,19 @@ func (self *Query) generateQuery() *mgo.Query {
 	}
 
 	return q
+}
+
+func (self *Query) getIdHex(val interface{}) (bson.ObjectId, error) {
+
+	myIdType := reflect.TypeOf(val)
+	myIdInstance := reflect.ValueOf(val)
+	myIdInstanceHex := myIdInstance.MethodByName("Hex")
+
+	if myIdType.Name() != "ObjectId" && myIdType.Kind() == reflect.String && val.(string) != "" {
+		return bson.ObjectIdHex(val.(string)), nil
+	} else if myIdType.Name() == "ObjectId" && myIdType.Kind() == reflect.String && val.(string) != "" {
+		return bson.ObjectIdHex(myIdInstanceHex.Call([]reflect.Value{})[0].String()), nil
+	} else {
+		return bson.NewObjectId(), errors.New(ERROR_INVALID_ID_VALUE)
+	}
 }
