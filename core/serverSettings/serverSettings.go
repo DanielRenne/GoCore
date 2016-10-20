9ff2469f7751934ba5fec60984d785663ffd8b11
Daniel Renne <Daniel.Renne@atlona.com>
2016-10-20 10:28:06 -0400

    Added Log Queries.

--- core/serverSettings/serverSettings.go
+++ core/serverSettings/serverSettings.go
@@ -56,6 +56,7 @@ type Application struct {
 	SessionSecureCookie   bool          `json:"sessionSecureCookie"`
 	CSRFSecret            string        `json:"csrfSecret"`
 	BootstrapData         bool          `json:"bootstrapData"`
+	LogQueries            bool          `json:"logQueries"`
 }
 
 type webConfigObj struct {
