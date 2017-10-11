fcf65498c83532ec12795837e237b8e3e6a5f4c9
David Renne <davidrenne@gmail.com>
2017-10-11 05:41:32 +0000

    Decoupling the appGen so that model build can be isolated to just RunDBCreate and never regenerate the app.

---
+++ modelBuild/modelBuild.go
@@ -0,0 +1,13 @@
+package modelBuild 
+
+import (
+	"github.com/DanielRenne/GoCore/core/dbServices"
+	"github.com/DanielRenne/GoCore/core/serverSettings"
+)
+
+func Initialize(path string, fileName string) {
+
+	serverSettings.Initialize(path, fileName)
+	dbServices.Initialize()
+	dbServices.RunDBCreate()
+}
