0c4d97e90730cfdcd65b53bd87bbe4a24b65e052
Daniel Renne <Daniel.Renne@atlona.com>
2017-11-22 00:01:19 -0500

    Removed Sync Object from all Models.

--- core/dbServices/createDBServices.go
+++ core/dbServices/createDBServices.go
@@ -863,7 +863,7 @@ func genNoSQLSchema(collectionName string, schema NOSQLSchema, driver string, sc
 		val += "} `json:\"Joins\" bson:\"-\"`\n\n"
 	}
 
-	val += "\n\t sync.RWMutex `bson:\"-\"`\n"
+	// val += "\n\t sync.RWMutex `bson:\"-\"`\n"
 
 	val += "\n}\n\n"
 
