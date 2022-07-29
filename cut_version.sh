7f396bb1d7eab67e88e91c67395b542f4ed4fcb6
David Renne <davidrenne@gmail.com>
2022-07-29 19:04:50 -0400

    Add gh release command for utility to cut releases.

--- cut_version.sh
+++ cut_version.sh
@@ -1,6 +1,8 @@
+set -e
 echo "New version number (with out the v)"
 read version
 git tag "v$version"
 git push origin "v$version"
-
-# todo: https://cli.github.com/manual/gh_release_create
+echo "Tag pushed, whats your title/description for the release?"
+read title
+gh release create "v$version" --title "$title"
