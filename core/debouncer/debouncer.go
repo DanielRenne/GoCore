bea396d788e6e920a5557f8ef7d1acb898e0b6b5
David Renne <davidrenne@gmail.com>
2023-01-21 11:21:30 -0500

    Add with Any parameters

--- core/debouncer/debouncer.go
+++ core/debouncer/debouncer.go
@@ -1,31 +1,54 @@
 package debouncer
 
 import (
+	"errors"
 	"sync"
 	"time"
 )
 
+const (
+	// ErrorTypeIncorrectSendSignal if you call SendSignal on something you configured WithAny
+	ErrorTypeIncorrectSendSignal = "You are using SendSignal with something setup WithAny"
+	// ErrorTypeIncorrectSendSignalWithAny  if you call SendSignalWithAny on something you configured WithTriggered
+	ErrorTypeIncorrectSendSignalWithAny = "You are using SendSignalWithAny with something setup WithTriggered"
+)
+
+// Debouncer main struct for debouncer package
 type Debouncer struct {
-	timeDuration  time.Duration
-	timer         *time.Timer
-	triggeredFunc func()
-	mu            sync.Mutex
-	done          chan struct{}
+	timeDuration     time.Duration
+	timer            *time.Timer
+	triggeredFunc    func()
+	triggeredAnyFunc func(any)
+	isAny            bool
+	mu               sync.Mutex
+	done             chan struct{}
 }
 
 // New creates a new instance of debouncer. Each instance of debouncer works independent, concurrency with different wait duration.
 func New(duration time.Duration) *Debouncer {
-	return &Debouncer{timeDuration: duration, triggeredFunc: func() {}}
+	return &Debouncer{timeDuration: duration, triggeredFunc: func() {}, triggeredAnyFunc: func(any) {}}
 }
 
 // WithTriggered attached a triggered function to debouncer instance and return the same instance of debouncer to use.
 func (d *Debouncer) WithTriggered(triggeredFunc func()) *Debouncer {
 	d.triggeredFunc = triggeredFunc
+	d.isAny = false
+	return d
+}
+
+// WithAny attached a triggered function to debouncer instance and return the same instance of debouncer to use.
+func (d *Debouncer) WithAny(triggeredFunc func(any)) *Debouncer {
+	d.triggeredAnyFunc = triggeredFunc
+	d.isAny = true
 	return d
 }
 
 // SendSignal makes an action that notifies to invoke the triggered function after a wait duration.
-func (d *Debouncer) SendSignal() {
+func (d *Debouncer) SendSignal() (err error) {
+	if d.isAny {
+		return errors.New(ErrorTypeIncorrectSendSignalWithAny)
+	}
+
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
@@ -37,12 +60,38 @@ func (d *Debouncer) SendSignal() {
 		}
 		d.done = make(chan struct{})
 	})
+	return nil
+}
+
+// SendSignalWithData makes an action that notifies to invoke the triggered function after a wait duration.
+func (d *Debouncer) SendSignalWithData(anyVar any) (err error) {
+	if !d.isAny {
+		return errors.New(ErrorTypeIncorrectSendSignal)
+	}
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.Cancel()
+	d.timer = time.AfterFunc(d.timeDuration, func() {
+		d.triggeredAnyFunc(anyVar)
+		if d.done != nil {
+			close(d.done)
+		}
+		d.done = make(chan struct{})
+	})
+	return nil
 }
 
 // Do run the signalFunc() and call SendSignal() after all. The signalFunc() and SendSignal() function run sequentially.
 func (d *Debouncer) Do(signalFunc func()) {
 	signalFunc()
 	d.SendSignal()
+}
+
+// DoAny run the signalFunc(any) and call SendSignalWithData(any) after all. The signalFunc() and SendSignal() function run sequentially.
+func (d *Debouncer) DoAny(signalFunc func(any), anyVar any) {
+	signalFunc(anyVar)
+	d.SendSignalWithData(anyVar)
 }
 
 // Cancel the timer from the last function SendSignal(). The scheduled triggered function is cancelled and doesn't invoke.
@@ -55,6 +104,11 @@ func (d *Debouncer) Cancel() {
 // UpdateTriggeredFunc replaces triggered function.
 func (d *Debouncer) UpdateTriggeredFunc(newTriggeredFunc func()) {
 	d.triggeredFunc = newTriggeredFunc
+}
+
+// UpdateAnyFunc replaces triggered function.
+func (d *Debouncer) UpdateAnyFunc(newTriggeredFunc func(any)) {
+	d.triggeredAnyFunc = newTriggeredFunc
 }
 
 // UpdateTimeDuration replaces the waiting time duration. You need to call a SendSignal() again to trigger a new timer with a new waiting time duration.
